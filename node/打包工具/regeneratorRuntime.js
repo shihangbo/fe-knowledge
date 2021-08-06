3c3752d3c9cbc7570cb20a8a6fcf65ad18ccf57f
shihangbo <shihangbo@126.com>
2021-08-06 11:27:30 +0800

    node

---
+++ node/打包工具/regeneratorRuntime.js
@@ -0,0 +1,52 @@
+
+// 原始代码
+function* generator() {
+  let a = yield 1
+  console.log(a)
+  let b = yield 2
+  console.log(b)
+  let c = yield 3
+  console.log(c)
+}
+let g = generator()
+console.log(g.next())
+console.log(g.next('11111'))
+console.log(g.next('22222'))
+console.log(g.next('33333'))
+
+// babel 转译后的代码
+"use strict";
+var _marked = /*#__PURE__*/regeneratorRuntime.mark(generator);
+function generator() {
+  var a, b, c;
+  return regeneratorRuntime.wrap(function generator$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return 1;
+        case 2:
+          a = _context.sent;
+          console.log(a);
+          _context.next = 6;
+          return 2;
+        case 6:
+          b = _context.sent;
+          console.log(b);
+          _context.next = 10;
+          return 3;
+        case 10:
+          c = _context.sent;
+          console.log(c);
+        case 12:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _marked);
+}
+var g = generator();
+console.log(g.next());
+console.log(g.next('11111'));
+console.log(g.next('22222'));
+console.log(g.next('33333'));
